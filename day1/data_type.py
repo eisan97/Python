2937787e9868db85bb2ed4caa8227d29496d2325
thineisan97 <umgodoodev9@gmail.com>
2023-11-27 10:55:17 +0630

    data type and fibonacci series

---
+++ day1/data_type.py
@@ -0,0 +1,13 @@
+# Data Types
+# 1.string
+# 2.integer
+# 3.float
+
+# Type Casting
+name = int(1000.005)
+print(type(name),name)
+
+# Sequence data types
+# item collection
+data = [1,2,3,4,'thin','ei','san']
+print(data)
